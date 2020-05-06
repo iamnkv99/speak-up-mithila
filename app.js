0b5e70b7ce95bb1ac25fd85cf367f568a87c2d98
iamnkv99 <niketverma99@gmail.com>
2020-05-06 20:02:29 +0530

    check

--- app.js
+++ app.js
@@ -74,133 +74,133 @@ app.use(express.static(__dirname + "/views"));
 // Load User model
 // app.use(flash());
 
-app.use(function (req, res, next) {
-  res.locals.currentUser = req.user;
-  res.locals.success_msg = req.flash("success_msg");
-  res.locals.error_msg = req.flash("error_msg");
-  res.locals.error = req.flash("error");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.locals.currentUser = req.user;
+//   res.locals.success_msg = req.flash("success_msg");
+//   res.locals.error_msg = req.flash("error_msg");
+//   res.locals.error = req.flash("error");
+//   next();
+// });
 ///////////
-var conn = mongoose.createConnection("mongodb://localhost/sum", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// var conn = mongoose.createConnection("mongodb://localhost/sum", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 //init gfs
-let gfs;
-conn.once("open", () => {
-  gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection("uploads");
-  // all set!
-});
+// let gfs;
+// conn.once("open", () => {
+//   gfs = Grid(conn.db, mongoose.mongo);
+//   gfs.collection("uploads");
+//   // all set!
+// });
 ////create storage
-const storage = new GridFsStorage({
-  url: "mongodb://localhost/sum",
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = buf.toString("hex") + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: "uploads",
-        };
-        resolve(fileInfo);
-      });
-    });
-  },
-});
-const upload = multer({ storage });
-////route
+// const storage = new GridFsStorage({
+//   url: "mongodb://localhost/sum",
+//   file: (req, file) => {
+//     return new Promise((resolve, reject) => {
+//       crypto.randomBytes(16, (err, buf) => {
+//         if (err) {
+//           return reject(err);
+//         }
+//         const filename = buf.toString("hex") + path.extname(file.originalname);
+//         const fileInfo = {
+//           filename: filename,
+//           bucketName: "uploads",
+//         };
+//         resolve(fileInfo);
+//       });
+//     });
+//   },
+// });
+// const upload = multer({ storage });
+// ////route
 
 app.get("/home", function (req, res) {
-  req.body.user;
+  // req.body.user;
 
-  gfs.files.find().toArray((err, files) => {
-    //checck if files exist
-    if (!files || files.length === 0) {
-      res.render("landing", { files: false });
-    } else {
-      files.map((file) => {
-        if (
-          file.contentType === "image/jpeg" ||
-          file.contentType === "image/png"
-        ) {
-          file.isImage = true;
-        } else {
-          file.isImage = false;
-        }
-      });
-      res.render("landing", { files: files });
-    }
-  });
+  // gfs.files.find().toArray((err, files) => {
+  //   //checck if files exist
+  //   if (!files || files.length === 0) {
+  //     res.render("landing", { files: false });
+  //   } else {
+  //     files.map((file) => {
+  //       if (
+  //         file.contentType === "image/jpeg" ||
+  //         file.contentType === "image/png"
+  //       ) {
+  //         file.isImage = true;
+  //       } else {
+  //         file.isImage = false;
+  //       }
+  //     });
+  res.render("landing", { files: false });
+  //   }
+  // });
 });
 /////
 ///
-app.post("/home", upload.single("file"), (req, res) => {
-  // res.json({file:req.file});
-  res.redirect("/home");
-});
+// app.post("/home", upload.single("file"), (req, res) => {
+//   // res.json({file:req.file});
+//   res.redirect("/home");
+// });
 ///rooute get /files
 ///displ;ay all files in json
-app.get("/files", (req, res) => {
-  gfs.files.find().toArray((err, files) => {
-    //checck if files exist
-    if (!files || files.length === 0) {
-      return res.status(404).json({
-        err: "nO files exist",
-      });
-    }
+// app.get("/files", (req, res) => {
+//   gfs.files.find().toArray((err, files) => {
+//     //checck if files exist
+//     if (!files || files.length === 0) {
+//       return res.status(404).json({
+//         err: "nO files exist",
+//       });
+//     }
 
-    ////files exist
-    return res.json(files);
-  });
-});
+//     ////files exist
+//     return res.json(files);
+//   });
+// });
 
-app.get("/files/:filename", (req, res) => {
-  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-    if (!file || file.length === 0) {
-      return res.status(404).json({
-        err: "nO files exist",
-      });
-    }
-    /////
-    return res.json(file);
-  });
-});
+// app.get("/files/:filename", (req, res) => {
+//   gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+//     if (!file || file.length === 0) {
+//       return res.status(404).json({
+//         err: "nO files exist",
+//       });
+//     }
+//     /////
+//     return res.json(file);
+//   });
+// });
 ////// get/image
 
-app.get("/image/:filename", (req, res) => {
-  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-    if (!file || file.length === 0) {
-      return res.status(404).json({
-        err: "nO files exist",
-      });
-    }
-    /////cgeck image
-    if (file.contentType === "image/jpeg" || file.contentType === "img/png") {
-      ////read outot to browser
-      const readstream = gfs.createReadStream(file.filename);
-      readstream.pipe(res);
-    } else {
-      res.status(404).json({
-        err: "not image",
-      });
-    }
-  });
-});
+// app.get("/image/:filename", (req, res) => {
+//   gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+//     if (!file || file.length === 0) {
+//       return res.status(404).json({
+//         err: "nO files exist",
+//       });
+//     }
+//     /////cgeck image
+//     if (file.contentType === "image/jpeg" || file.contentType === "img/png") {
+//       ////read outot to browser
+//       const readstream = gfs.createReadStream(file.filename);
+//       readstream.pipe(res);
+//     } else {
+//       res.status(404).json({
+//         err: "not image",
+//       });
+//     }
+//   });
+// });
 
 ////routes delete
-app.delete("/files/:id", (req, res) => {
-  gfs.remove({ _id: req.params.id, root: "uploads" }, (err, gridStore) => {
-    if (err) {
-      return res.status(404).json({ err: err });
-    }
-    res.redirect("/home");
-  });
-});
+// app.delete("/files/:id", (req, res) => {
+//   gfs.remove({ _id: req.params.id, root: "uploads" }, (err, gridStore) => {
+//     if (err) {
+//       return res.status(404).json({ err: err });
+//     }
+//     res.redirect("/home");
+//   });
+// });
 
 ////////////////
 
@@ -227,86 +227,86 @@ app.get("/upcoming", function (req, res) {
 app.get("/past", function (req, res) {
   res.render("past");
 });
-app.get("/login", (req, res) => res.render("login"));
-app.post("/register", function (req, res) {
-  const { name, email, password, password2 } = req.body;
-  let errors = [];
-  //check requird fields
-  if (!name || !email || !password || !password2) {
-    errors.push({ msg: "please fill in" });
-  }
-  //chech passwoerd match
-  if (password !== password2) {
-    errors.push({ msg: "password incorrect" });
-  }
-  //chech password lenght
-  if (password.length < 6) {
-    errors.push({ msg: "password should be at least 6 character" });
-  }
+// app.get("/login", (req, res) => res.render("login"));
+// app.post("/register", function (req, res) {
+//   const { name, email, password, password2 } = req.body;
+//   let errors = [];
+//   //check requird fields
+//   if (!name || !email || !password || !password2) {
+//     errors.push({ msg: "please fill in" });
+//   }
+//   //chech passwoerd match
+//   if (password !== password2) {
+//     errors.push({ msg: "password incorrect" });
+//   }
+//   //chech password lenght
+//   if (password.length < 6) {
+//     errors.push({ msg: "password should be at least 6 character" });
+//   }
 
-  if (errors.length > 0) {
-    res.render("register", {
-      errors,
-      name,
-      email,
-      password,
-      password2,
-    });
-  } else {
-    // res.send('pass');
-    User.findOne({ email: email }).then((user) => {
-      if (user) {
-        errors.push({ msg: "Email already exists" });
-        res.render("register", {
-          errors,
-          name,
-          email,
-          password,
-          password2,
-        });
-      } else {
-        const newUser = new User({
-          name,
-          email,
-          password,
-        });
+//   if (errors.length > 0) {
+//     res.render("register", {
+//       errors,
+//       name,
+//       email,
+//       password,
+//       password2,
+//     });
+//   } else {
+//     // res.send('pass');
+//     User.findOne({ email: email }).then((user) => {
+//       if (user) {
+//         errors.push({ msg: "Email already exists" });
+//         res.render("register", {
+//           errors,
+//           name,
+//           email,
+//           password,
+//           password2,
+//         });
+//       } else {
+//         const newUser = new User({
+//           name,
+//           email,
+//           password,
+//         });
 
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser
-              .save()
-              .then((user) => {
-                req.flash(
-                  "success_msg",
-                  "You are now registered and can log in"
-                );
-                res.redirect("login");
-              })
-              .catch((err) => console.log(err));
-          });
-        });
-      }
-    });
-  }
-});
+//         bcrypt.genSalt(10, (err, salt) => {
+//           bcrypt.hash(newUser.password, salt, (err, hash) => {
+//             if (err) throw err;
+//             newUser.password = hash;
+//             newUser
+//               .save()
+//               .then((user) => {
+//                 req.flash(
+//                   "success_msg",
+//                   "You are now registered and can log in"
+//                 );
+//                 res.redirect("login");
+//               })
+//               .catch((err) => console.log(err));
+//           });
+//         });
+//       }
+//     });
+//   }
+// });
 
 // Login
-app.post("/login", (req, res, next) => {
-  passport.authenticate("local", {
-    successRedirect: "/home",
-    failureRedirect: "/login",
-    failureFlash: true,
-  })(req, res, next);
-});
+// app.post("/login", (req, res, next) => {
+//   passport.authenticate("local", {
+//     successRedirect: "/home",
+//     failureRedirect: "/login",
+//     failureFlash: true,
+//   })(req, res, next);
+// });
 
 // Logout
-app.get("/logout", (req, res) => {
-  req.logout();
-  req.flash("success_msg", "You are logged out");
-  res.redirect("/login");
-});
+// app.get("/logout", (req, res) => {
+//   req.logout();
+//   req.flash("success_msg", "You are logged out");
+//   res.redirect("/login");
+// });
 
 ///////log in ///
 // app.get("/login", function(req, res){
@@ -324,12 +324,12 @@ app.get("/logout", (req, res) => {
 //     res.redirect("home");
 // })
 ///middleware function///
-function isLoggedin(req, res, next) {
-  if (req.isAunthenticated()) {
-    return next();
-  }
-  res.redirect("/login");
-}
+// function isLoggedin(req, res, next) {
+//   if (req.isAunthenticated()) {
+//     return next();
+//   }
+//   res.redirect("/login");
+// }
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("SpeakUpMithla");
